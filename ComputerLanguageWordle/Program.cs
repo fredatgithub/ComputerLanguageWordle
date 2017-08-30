b1320418eb59cd2cf08866e09b99410c92bcec9b
Fred <domaine75@gmail.com>
2017-08-30 14:02:07 +0200

    Add xml file and code to read it

--- ComputerLanguageWordle/Program.cs
+++ ComputerLanguageWordle/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml;
 
 namespace ComputerLanguageWordle
 {
@@ -10,7 +13,7 @@ namespace ComputerLanguageWordle
     {
       Action<string> display = Console.WriteLine;
       var computerLanguageList = new List<string>();
-      computerLanguageList.AddRange(GetElements("ComputerLanguages.xml", "language"));
+      computerLanguageList.AddRange(GetElements("ComputerLanguages.xml", "Languages"));
 
       display("Computer language wordle");
 
@@ -28,13 +31,26 @@ namespace ComputerLanguageWordle
 
       try
       {
-        
+        result = ParseXmlToList(fileName, tag);
 
       }
       catch (Exception exception)
       {
         Console.WriteLine($"An error occured: {exception.Message}");
         return result;
+      }
+
+      return result;
+    }
+
+    private static IEnumerable<string> ParseXmlToList(string fileName, string firstXmlTag)
+    {
+      var result = new List<string>();
+      using (var reader = XmlReader.Create(new StringReader(fileName)))
+      {
+        reader.ReadToFollowing(firstXmlTag);
+        string genre = reader.Value;
+        result.Add(genre);
       }
 
       return result;
