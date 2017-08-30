19988bee4ad41481c840e78d82f99a827f84a7ed
Fred <domaine75@gmail.com>
2017-08-30 13:47:51 +0200

    Initial commit

---
+++ ComputerLanguageWordle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace ComputerLanguageWordle
+{
+  internal static class Program
+  {
+    private static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      var computerLanguageList = new List<string>();
+      computerLanguageList.AddRange(GetElements("ComputerLanguages.xml", "language"));
+
+      display("Computer language wordle");
+
+      display("Press any key to exit: ");
+      Console.ReadKey();
+    }
+
+    private static IEnumerable<string> GetElements(string fileName, string tag)
+    {
+      IEnumerable<string> result = new List<string>();
+      if (string.IsNullOrEmpty(fileName))
+      {
+        return result;
+      }
+
+      try
+      {
+        
+
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine($"An error occured: {exception.Message}");
+        return result;
+      }
+
+      return result;
+    }
+  }
+}
